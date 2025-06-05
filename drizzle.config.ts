68838b7146ebe23487ba7bb905a9cd43abede225
daveclinton <clintondavid46@gmail.com>
2025-06-05 11:01:54 +0300

    feat: add supabase color theme

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,8 +2,11 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL ?? "");
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in the environment variables");
+}
 
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, {
   schema,
   logger: process.env.NODE_ENV === "development" ? true : false,
