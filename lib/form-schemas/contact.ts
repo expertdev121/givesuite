68219e3228178ea36bca0581f3f3dcc1bdc8618e
daveclinton <clintondavid46@gmail.com>
2025-07-03 10:22:39 +0300

    feat: add contact file

--- lib/form-schemas/contact.ts
+++ lib/form-schemas/contact.ts
@@ -10,16 +10,8 @@ export const contactFormSchema = z.object({
     .min(2, { message: "Last name must be at least 2 characters" })
     .max(32, { message: "Last name must be at most 32 characters" }),
   email: z.string().email({ message: "Invalid email address" }),
-  phone: z
-    .string()
-    .min(7, { message: "Phone number must be at least 7 characters" })
-    .max(15, { message: "Phone number must be at most 15 characters" })
-    .optional(),
+  phone: z.string().optional(),
   title: z.enum(["mr", "mrs", "ms", "dr", "prof", "eng", "other"]).optional(),
   gender: z.enum(["male", "female"]).optional(),
-  address: z
-    .string()
-    .min(5, { message: "Address must be at least 5 characters" })
-    .max(255, { message: "Address must be at most 255 characters" })
-    .optional(),
+  address: z.string().optional(),
 });
