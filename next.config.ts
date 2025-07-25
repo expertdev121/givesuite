0be9fb1b9d6d456dd00c40fa66b52cc11f2b7553
vishal <jhavishal523@gmail.com>
2025-07-25 11:43:43 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   experimental: {
+    optimizeCss: false, //  disables lightningcss to avoid .node binary error
+  },
 };
-
+module.exports = nextConfig;
 export default nextConfig;
