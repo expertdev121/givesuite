9d2ffd6bf49b264a00d4d65567f95e59767546e2
vishal <jhavishal523@gmail.com>
2025-07-25 12:23:33 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  experimental: {
-    optimizeCss: false, // disables lightningcss
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-export default nextConfig;
 
+export default nextConfig;
